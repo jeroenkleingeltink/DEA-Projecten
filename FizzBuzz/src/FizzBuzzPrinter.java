028d25e0fbfe9ebbeb4adaaf7183c3ac50ebd0c4
Jeroen Klein Geltink <jeroen_klein_geltink@hotmail.com>
2018-09-05 10:41:05 +0200

    FizzBuzz starting project

---
+++ FizzBuzz/src/FizzBuzzPrinter.java
@@ -0,0 +1,36 @@
+class FizzBuzzPrinter {
+    /**
+     * Functions that prints Fizz, Buzz or number up to given parameter maximum
+     * @param counter - maximum number for which FizzBuzzPrinter will be calculated
+     */
+    static void PrintFizzBuzz(int counter) {
+        for (int i = 0; i <= counter; i++) {
+
+            String result;
+
+            if ( i % 15 == 0) {
+                // fizzbuzz
+                result = "FizzBuzzPrinter";
+            } else if (i % 3 == 0) {
+                // fizz
+                result = "Fizz";
+            } else if (i % 5 == 0) {
+                // buzz
+                result = "Buzz";
+            } else {
+                // print cijfer
+                result = Integer.toString(i);
+            }
+
+            Write(result);
+        }
+    }
+
+    /**
+     * Function that prints to console
+     * @param result - Result that has to be printed
+     */
+    private static void Write(String result) {
+        System.out.println(result);
+    }
+}
