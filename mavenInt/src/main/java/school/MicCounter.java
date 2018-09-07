08c616d2ae6773aef1afba6b9451315c65debee0
Jeroen Klein Geltink <jeroen_klein_geltink@hotmail.com>
2018-09-07 14:56:37 +0200

    WIP

---
+++ mavenInt/src/main/java/school/MicCounter.java
@@ -0,0 +1,19 @@
+package school;
+
+public class MicCounter implements Runnable {
+
+    private MicInt micint;
+    private static int counts = 1000;
+
+    public MicCounter(MicInt mi) {
+        this.micint = mi;
+    }
+
+    public void count() {
+        for (int i = 0; i < )
+    }
+
+    public void run() {
+
+    }
+}
